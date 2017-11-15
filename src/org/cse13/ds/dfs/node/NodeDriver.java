4eb51f86883beed781b753265060c665f70dac0f
dinuka <dinukasal@gmail.com>
2017-11-15 05:30:15 +0000

    set local ip from args

--- src/org/cse13/ds/dfs/node/NodeDriver.java
+++ src/org/cse13/ds/dfs/node/NodeDriver.java
@@ -16,30 +16,31 @@ public class NodeDriver {
     public static void main(String[] args) throws IOException, NotBoundException {
 
         // getting local ip
-        String ip="";
+        String ip = "";
 
         try {
             Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
-            int position =0;
+            int position = 0;
             while (interfaces.hasMoreElements()) {
                 NetworkInterface iface = interfaces.nextElement();
                 //This filters out the interfaces
                 // filters out 127.0.0.1 and inactive interfaces
-                if (iface.isLoopback() || !iface.isUp())
+                if (iface.isLoopback() || !iface.isUp()) {
                     continue;
+                }
 
                 Enumeration<InetAddress> addresses = iface.getInetAddresses();
-                while(addresses.hasMoreElements()) {
+                while (addresses.hasMoreElements()) {
                     InetAddress addr = addresses.nextElement();
-                    if (addr instanceof Inet6Address) continue;
+                    if (addr instanceof Inet6Address) {
+                        continue;
+                    }
                     ip = addr.getHostAddress();
                     position++;
-                    
-                    if (position == 0 ) {
-                        System.out.println(iface.getDisplayName() + " " + ip);
-                    }
 
-                    else{
+                    if (position == 0) {
+                        System.out.println(iface.getDisplayName() + " " + ip);
+                    } else {
                         break;
                     }
                 }
@@ -47,8 +48,14 @@ public class NodeDriver {
         } catch (SocketException e) {
             throw new RuntimeException(e);
         }
-        
-        System.out.println("IP Address:"+ip);
+
+        try {
+            System.out.println(args[0]);
+            ip =args[0];
+        } catch (Exception e) {
+
+        }
+        System.out.println("IP Address:" + ip);
 
         Node n1 = new Node(ip);
 
